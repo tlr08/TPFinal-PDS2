e2166f4b43ccfc8b186099637ce3c37c195a8eaa
Lucas Chaves <lucasxp205@gmail.com>
2018-12-02 10:56:02 -0200

    DbHelper adicionado.

--- program/main.cpp
+++ program/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 	DbHelper *db; 	
-	db = new DbHelper("gerenciadorResiduos.db");
+	db = new DbHelper(DEFAULT_DBNAME);
 	db->startConnection();
 	cout << "Running db: " << db->getDbName() << endl;
 	db->closeConnection();
