6b5e7fd95eca14985250455551d5496bd90be253
ibispera <lucasbhbispo@gmail.com>
2018-12-06 19:21:07 -0200

    Adicionado Cadastro do Ponto de Coleta

--- include/models/PontoColeta.hpp
+++ include/models/PontoColeta.hpp
@@ -1,8 +1,30 @@
-#ifndef PONTOCOLETA_HPP
-#define PONTOCOLETA_HPP
-#include "stdafx.hpp"
+#ifndef PONTOCOLETA_H
+#define PONTOCOLETA_H
+#include "Usuario.h"
+#include <string>
 using namespace std;
 
-class Ponto Coleta {};
+class PontoColeta
+{
+    public:
+        PontoColeta();
+        PontoColeta(string nome,string endereco /*Usuario *User*/);
+        string get_nome() const;
+        string get_endereco() const;
+        //Usuario* get_usuario();
+        bool set_nome(string nome);
+        bool set_endereco(string endereco);
+     //   bool set_Usuario(Usuario *User);
+        ~PontoColeta();
+        friend ostream& operator<<(ostream& out,const PontoColeta& obj);
+        friend istream& operator>>(istream& in,PontoColeta& obj);
 
-#endif
+    protected:
+
+    private:
+        string _nome, _endereco;
+     //   Usuario *_User;
+
+};
+
+#endif // PONTOCOLETA_H
---
+++ src/models/PontoColeta.cpp
@@ -0,0 +1,86 @@
+#include "PontoColeta.h"
+#include <iostream>
+        PontoColeta::PontoColeta()
+        {
+            _nome = "";
+            _endereco = "";
+            //Usuario *_User;
+        }
+        PontoColeta::PontoColeta(string nome,string endereco /*Usuario *User*/)
+        {
+            _nome = nome;
+            _endereco = endereco;
+          //  _User = User;
+        }
+        string PontoColeta::get_nome() const
+        {
+            return this->_nome;
+        }
+        string PontoColeta::get_endereco() const
+        {
+            return this->_endereco;
+        }
+      /*  Usuario* PontoColeta::get_usuario()
+        {
+           return *_User;
+        }*/
+        bool PontoColeta::set_nome(string nome)
+        {
+            if(!nome.empty()){
+                _nome = nome;
+                return true;
+            }
+            return false;
+        }
+        bool PontoColeta::set_endereco(string endereco)
+        {
+            if(!endereco.empty()){
+                _endereco = endereco;
+                return true;
+            }
+            return false;
+        }
+       /* bool PontoColeta::set_Usuario(Usuario User);
+        {
+
+        }*/
+
+        PontoColeta::~PontoColeta()
+        {
+            _nome.clear();
+            _endereco.clear();
+        }
+
+         ostream& operator<<(ostream& out,const PontoColeta& obj)
+        {
+            out << "Nome: " << obj.get_nome() << endl;
+           // out << "Usuário: " << (obj.get_usuario())->get_nome_usuario << endl;
+            out << "Endereco: " << obj.get_endereco() << endl;
+            out << endl;
+            return out;
+        }
+         istream& operator>>(istream& in,PontoColeta& obj)
+        {
+            string nome = "";
+            string endereco = "";
+
+
+            clearBuffer(in);
+
+
+            cout << "Informe o nome do Local: ";
+            getline(in,nome,'\n');
+
+         /*   cout << "Informe o usuario: ";
+            getline(in,nome,'\n');*/
+
+            cout << "Informe o endereço: ";
+            getline(in,endereco,'\n');
+
+
+            obj.set_nome(nome);
+           // obj.set_usuario(nome_usuario);
+            obj.set_endereco(endereco);
+
+            return in;
+        }
